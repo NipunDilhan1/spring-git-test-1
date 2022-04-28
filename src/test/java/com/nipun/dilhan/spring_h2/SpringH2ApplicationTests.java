c8ba7d80d9f41a331707ca94791a607ae523d84d
Nipun Dilhan <nipundilhanjayarathne@gmail.com>
2022-04-28 21:11:51 +0530

    first commit initial project

---
+++ src/test/java/com/nipun/dilhan/spring_h2/SpringH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nipun.dilhan.spring_h2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
