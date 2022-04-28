841955095bf443fda265390f543cfb9137f29e8f
Nipun Dilhan <nipundilhanjayarathne@gmail.com>
2022-04-28 22:31:07 +0530

    changes commited again

--- src/main/java/com/nipun/dilhan/spring_h2/controller/TestController1.java
+++ src/main/java/com/nipun/dilhan/spring_h2/controller/TestController1.java
@@ -4,25 +4,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController  
-@RequestMapping("/test-1") 
-public class TestController1 {
+public class TestController1 { 
 	
-<<<<<<< Updated upstream
-	@RequestMapping("/welcome")  
-	public String welcome()   {  
-		
-		return "Hello welcome to git/ spring world";  
-	
-	} 
-	
+
 	@RequestMapping("/")  
-=======
-	@RequestMapping("/hello")  
->>>>>>> Stashed changes
 	public String hello()   {  
-		
-		
-		
+				
 		return "Hello javaTpoint";  
 	
 	} 
