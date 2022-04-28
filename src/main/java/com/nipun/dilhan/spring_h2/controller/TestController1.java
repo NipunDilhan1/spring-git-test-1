fa618f11cb2d143cdc8db199f0baeab88a848de7
nipun-dilhan <nipundilhanjayarathne@gmail.com>
2022-04-28 10:01:58 -0700

    new method added

--- src/main/java/com/nipun/dilhan/spring_h2/controller/TestController1.java
+++ src/main/java/com/nipun/dilhan/spring_h2/controller/TestController1.java
@@ -6,6 +6,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController  
 public class TestController1 { 
 	
+	@RequestMapping("/welcome")  
+	public String welcome()   {  
+		
+		return "Hello welcome to git/ spring world";  
+	
+	} 
 
 	@RequestMapping("/")  
 	public String hello()   {  
