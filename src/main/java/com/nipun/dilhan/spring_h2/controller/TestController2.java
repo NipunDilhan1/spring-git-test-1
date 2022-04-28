f88f63e889b289cc90d5e6f0423d06c65176010c
Nipun Dilhan <nipundilhanjayarathne@gmail.com>
2022-04-28 23:00:33 +0530

    Update TestController2.java

--- src/main/java/com/nipun/dilhan/spring_h2/controller/TestController2.java
+++ src/main/java/com/nipun/dilhan/spring_h2/controller/TestController2.java
@@ -10,23 +10,29 @@ public class TestController2 {
 	@RequestMapping("/welcome")  
 	public String welcome()   {  
 		
+
 		System.out.println(" added by nipundilhanjayarathne ");
 		System.out.println("  ");
+
 		System.out.println("added by nipun.dilhan1 ");
 		System.out.println(" ");
+
 		
 		return "Hello welcome 2nd controller";   
 	
 	} 
 	
+
 	@RequestMapping("/n-d-j")  
 	public String nipundilhanjayarathne()   {  
 		
 		return "Hello nipun dilhan jayarathne";  
+
 	}
 	
 	@RequestMapping("/n-d")  
 	public String nipundilhan()   {  
+		
 		return "hello nipun dilhan";  
 	
 	} 
@@ -34,7 +40,9 @@ public class TestController2 {
 	@RequestMapping("/second")  
 	public String hello()   {  
 		
+
 		return "Hello second controller n-d ";  
+
 	
 	} 
 
