536783ce47b7a6239f2aa054167976ae6eb81097
nipun-dilhan <nipundilhanjayarathne@gmail.com>
2022-04-28 10:23:51 -0700

    2nd changes added to the second controller

--- src/main/java/com/nipun/dilhan/spring_h2/controller/TestController2.java
+++ src/main/java/com/nipun/dilhan/spring_h2/controller/TestController2.java
@@ -10,14 +10,24 @@ public class TestController2 {
 	@RequestMapping("/welcome")  
 	public String welcome()   {  
 		
+		System.out.println(" added by nipundilhanjayarathne ");
+		System.out.println("  ");
+		
 		return "Hello welcome 2nd controller";   
+	
+	} 
+	
+	@RequestMapping("/n-d-j")  
+	public String nipundilhanjayarathne()   {  
+		
+		return "Hello nipun dilhan jayarathne";  
 	
 	} 
 	
 	@RequestMapping("/second")  
 	public String hello()   {  
 		
-		return "Hello second controller";  
+		return "Hello second controller n-d-j";  
 	
 	} 
 
